0ae4de2f3621adc2ec8566872e0166e78af4af02
Nelson Santisteban <nelson_la76@yahoo.com>
2020-04-17 18:10:03 -0500

    agrego todos los archivos

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HolaMundoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
